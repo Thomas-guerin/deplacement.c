521cb20a8bbd94d935fef8d354a30a5f2c4a65a4
Thomas-guerin <59953522+Thomas-guerin@users.noreply.github.com>
2020-02-06 09:30:52 +0100

    Update jdvelh.c

--- jdvelh.c
+++ jdvelh.c
@@ -4,12 +4,25 @@
 
 
 // choix joueur 
-int choix
+	int choix
+	
+	struct chapitre {
 
-int main() 
+		char description;
+		int or;
+		int soin;
+		int tabchap[10];
+};
+
+typedef struct chapitre chapitre;
+
+	int main() 
 {
-
-printf("Bonjour aventurier , choisissez votre destination . Voulez vous aller vers Bordemer ou Vertpré ?\n");
+	chapitre chapitre1 = {"Initiation",5,+20,{2,3,4}};
+	chapitre chapitre2 = {"Lataverne",-5,2,{3,4,5}};
+	chapitre chapitre3 = {"Larencontre",20,+5,{4,5,6}};
+printf("%s\n",chapitre1);
+printf("Bonjour aventurier , vous voici dans l'Initiation, choisissez votre destination . Voulez vous aller vers Bordemer ou Vertpré ?\n");
 printf("Tapez 1 pour Bordemer ou 2 pour Vertpré\n");
 scanf("%d" ,%choix); 
 	if ( choix==1)
@@ -20,4 +33,9 @@ scanf("%d" ,%choix);
 	{
 		printf("Bienvenu a Vertpré ! \n");
 	}
-}
+
+
+
+	
+
+
