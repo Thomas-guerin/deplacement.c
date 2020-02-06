1a1a9c38b0b0afe146a8cec97bf01e2337f0f2b8
Thomas-guerin <59953522+Thomas-guerin@users.noreply.github.com>
2020-02-06 09:06:04 +0100

    Update jdvelh.c

--- jdvelh.c
+++ jdvelh.c
@@ -2,5 +2,22 @@
 #include <stdlib.h>
 #include <string.h>
 
-char Bordemer
-char Vertpré 
+
+// choix joueur 
+int choix
+
+int main() 
+{
+
+printf("Bonjour aventurier , choisissez votre destination . Voulez vous aller vers Bordemer ou Vertpré ?\n");
+printf("Tapez 1 pour Bordemer ou 2 pour Vertpré\n");
+scanf("%d" ,%choix); 
+	if ( choix==1)
+	{
+		printf("Bienvenu a Bordemer ! \n");
+	}
+	else if ( choix==2 )
+	{
+		printf("Bienvenu a Vertpré ! \n");
+	}
+}
